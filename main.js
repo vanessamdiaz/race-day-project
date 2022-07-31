96bda37e50d08316f359a5cedca0ed01e41e5424
vanessamdiaz <109388381+vanessamdiaz@users.noreply.github.com>
2022-07-31 10:44:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 let raceNumber = Math.floor(Math.random() * 1000);
-const earlyraceNumber = raceNumber + 1000;
-const earlyRegister = 'True'
-const runnerAge = 17
-const randomNumber = Math.floor(Math.random() * 8);
-const eightBall = randomNumber;
 
-if (runnerAge > 18 && earlyRegister == 'True') {
-    console.log(`You will race at 9:30 am and your race number is ${earlyraceNumber}.`);
-}   else if (runnerAge > 18 && earlyRegister != 'True') {
+let earlyRegister = true
+let runnerAge = 25
+
+if(runnerAge > 18 && earlyRegister === true) {
+  raceNumber += 1000;  
+}
+
+console.log(`Runner age is ${runnerAge}`);
+console.log(`Runner registered early: ${earlyRegister}`);
+
+
+if (runnerAge > 18 && earlyRegister === true) {
+    console.log(`You will race at 9:30 am and your race number is ${raceNumber}.`);
+}   else if (runnerAge > 18 && earlyRegister === false) {
     console.log(`You will race at 11:00 am and your race number is ${raceNumber}.`);
 }   else if (runnerAge < 18) {
     console.log(`You will race at 12:30 pm and your race number is ${raceNumber}.`);
